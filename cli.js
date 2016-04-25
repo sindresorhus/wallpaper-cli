0d84812d95eb55c3944f7aab376c7ff7658bd24d
rawgni <ingwar.wirjawan@gmail.com>
2016-04-25 09:52:57 +0700

    fix lint issues and update cli help

--- cli.js
+++ cli.js
@@ -1,21 +1,22 @@
 #!/usr/bin/env node
 'use strict';
+const fs = require('fs');
+const path = require('path');
 const meow = require('meow');
 const wallpaper = require('wallpaper');
 const isUrl = require('is-url-superb');
 const got = require('got');
 const tempfile = require('tempfile');
-const path = require('path');
-const fs = require('fs');
 
 const cli = meow(`
 	Usage
-	  $ wallpaper [file]
+	  $ wallpaper [file | url]
 
 	Example
 	  $ wallpaper unicorn.jpg
 	  $ wallpaper
 	  /Users/sindresorhus/unicorn.jpg
+	  $ wallpaper https://assets-cdn.github.com/images/modules/open_graph/github-octocat.png
 `, {
 	string: ['_']
 });
@@ -23,17 +24,18 @@ const cli = meow(`
 const input = cli.input[0];
 
 if (input) {
-  if (isUrl(input)) {
-    var file = tempfile(path.extname(input));
-    got
-      .stream(input)
-      .pipe(fs.createWriteStream(file))
-      .on('finish', () => {
-        wallpaper.set(file);
-      });
-  } else {
-	  wallpaper.set(input);
-  }
+	if (isUrl(input)) {
+		const file = tempfile(path.extname(input));
+		got
+			.stream(input)
+			.pipe(fs.createWriteStream(file))
+			.on('finish', () => {
+				console.log (file);
+				wallpaper.set(file);
+			});
+	} else {
+		wallpaper.set(input);
+	}
 } else {
 	wallpaper.get().then(console.log);
 }
