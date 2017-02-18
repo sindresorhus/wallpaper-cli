3df957938e4167b1257675562e849d9c5d2023a7
Jakob Krigovsky <jakob@krigovsky.com>
2017-02-18 18:31:17 +0100

    Add support for scaling method

--- cli.js
+++ cli.js
@@ -12,16 +12,23 @@ const cli = meow(`
 	Usage
 	  $ wallpaper [file|url]
 
+	  Options
+	    --scale  Scaling method (fill fit stretch center)
+	             If not specified, it will use your current setting
+	             Only available on macOS
+
 	Examples
 	  $ wallpaper unicorn.jpg
 	  $ wallpaper https://octodex.github.com/images/dojocat.jpg
 	  $ wallpaper
 	  /Users/sindresorhus/unicorn.jpg
+	  $ wallpaper codercat.jpg --scale fit
 `, {
 	string: ['_']
 });
 
 const input = cli.input[0];
+const scale = cli.flags.scale;
 
 if (input) {
 	if (isUrl(input)) {
@@ -31,10 +38,10 @@ if (input) {
 			.stream(input)
 			.pipe(fs.createWriteStream(file))
 			.on('finish', () => {
-				wallpaper.set(file);
+				wallpaper.set(file, {scale});
 			});
 	} else {
-		wallpaper.set(input);
+		wallpaper.set(input, {scale});
 	}
 } else {
 	wallpaper.get().then(console.log);
