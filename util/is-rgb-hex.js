516fd8a3f0b24b26df51dfa5417a859576b12f57
Sindre Sorhus <sindresorhus@gmail.com>
2021-05-28 13:41:44 +0700

    Update is-rgb-hex.js

--- util/is-rgb-hex.js
+++ util/is-rgb-hex.js
@@ -2,9 +2,8 @@ const isRGBHex = hex => {
 	if (typeof hex !== 'string') {
 		return false;
 	}
-	const hexPattern = /^#?([A-Fa-f0-9]{3}([A-Fa-f0-9]{3})?)$/;
-	const result = hexPattern.test(hex);
-	return result;
+
+	return /^#?([A-Fa-f0-9]{3}([A-Fa-f0-9]{3})?)$/.test(hex);
 };
 
 module.exports = isRGBHex;
